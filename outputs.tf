f46eddcde837c88f3381ad85aafbc21618fdb073
root <root@ip-172-31-40-113.ap-south-1.compute.internal>
2024-02-21 09:26:22 +0000

    all

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 output "target_group_arns" {
   value = aws_lb_target_group.main.arn
 }
+
+
+output "app_alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
+}
+
+
+
+output "app_alb_zone_id" {
+  value = aws_lb.app_alb.zone_id
+}
